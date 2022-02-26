4c7b4fea431b6f3d75b1ec56f086d5dc0467bb03
MDSHOHAG617 <95575228+MDSHOHAG617@users.noreply.github.com>
2022-02-26 20:31:35 +0600

    almost done

--- Api.js
+++ Api.js
@@ -1,3 +1,47 @@
+/* Preloading Results */
+const inputFeild = document.getElementById('inputFeild');
+const input = inputFeild.value;
+// console.log(input)
+inputFeild.value = "";
+
+
+const url = `https://themealdb.com/api/json/v1/1/search.php?s=${input}`;
+fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResult(data.meals))
+
+
+const displaySearchResult = meals => {
+    const searchResult = document.getElementById('search-result');
+    searchResult.innerHTML = '';
+    if (meals.length == 0) {
+        alert("No Result Found")
+    }
+    // searchResult.textContent = '';
+    meals.forEach(meal => {
+        // console.log(meal)
+        const div = document.createElement('div');
+        div.className = "col";
+        div.innerHTML = `<div class="col">
+            <div onclick="loadMealDetaile(${meal.idMeal})" class="card ">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+                </div>
+            </div>
+        </div>`;
+
+        searchResult.appendChild(div)
+
+
+    });
+}
+/* Preloading Results Ends */
+
+
+
+/* load search by cliking search button  */
 const loadSearch = () => {
     const inputFeild = document.getElementById('inputFeild');
     const input = inputFeild.value;
